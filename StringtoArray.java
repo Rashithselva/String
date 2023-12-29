9e135fa819dc216705cd050d0ef63a21a89b6cab
Rashithselva <ravirashith@gmail.com>
2023-12-29 19:02:09 +0530

    convert String to Array

--- StringtoArray.java
+++ StringtoArray.java
@@ -1,5 +1,13 @@
 package String;
 
 public class StringtoArray {
+    public static void main(String[] args) {
+        String res="hi selva";
+        char a[]=res.toCharArray();
+
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
     
 }
