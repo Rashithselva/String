1de38740d68f3326f632bd621d03d2cc4cd77170
Rashithselva <ravirashith@gmail.com>
2023-12-30 08:58:39 +0530

    String add in to array

--- Annagram.java
+++ Annagram.java
@@ -1,6 +1,27 @@
 package String;
+import java.util.Arrays;
 
 public class Annagram {
-  
-    
+      public static void main(String[] args) {
+        
+        String res="is my boy";
+        String m1="",m2="",m3="",m4="",m5="",m6="";
+        String a[]=res.split(" ");
+
+        for(int i=0;i<a.length;i++){
+            if(i==0){
+                m1+=a[i];
+                
+            }
+            if(i==1){
+                m2+=a[i];
+               
+            }
+            if(i==2){
+                m3+=a[i];
+                
+            }
+        }
+        // System.out.println(Arrays.toString(a));
+    }  
 }
