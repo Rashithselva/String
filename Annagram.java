18659e14f6e01a9c32d7a5da3af09270f02307ee
Rashithselva <ravirashith@gmail.com>
2023-12-30 09:03:33 +0530

    Converted to annagram

--- Annagram.java
+++ Annagram.java
@@ -11,17 +11,34 @@ public class Annagram {
         for(int i=0;i<a.length;i++){
             if(i==0){
                 m1+=a[i];
-                
+                for(int j=m1.length()-1;j>=0;j--){
+                    m4+=m1.charAt(j);
+                }
             }
             if(i==1){
                 m2+=a[i];
-               
+                for(int j=m2.length()-1;j>=0;j--){
+                    m5+=m2.charAt(j);
+                }
             }
             if(i==2){
                 m3+=a[i];
-                
+                for(int j=m3.length()-1;j>=0;j--){
+                    m6+=m3.charAt(j);
+                }
             }
         }
-        // System.out.println(Arrays.toString(a));
+        for(int i=0;i<a.length;i++){
+            if(i==0){
+                a[i]=m4;
+            }
+            if(i==1){
+                a[i]=m5;
+            }
+            if(i==2){
+                a[i]=m6;
+            }
+        }
+        System.out.println(Arrays.toString(a));
     }  
 }
